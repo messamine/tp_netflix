fafe7764006d1266de38c3f0a49ca019ea73aabd
WALAA ALLAM <walaa221allam@gmail.com>
2022-02-07 12:01:34 +0100

    test

--- MODE_deconnecter/frm_genre.Designer.cs
+++ MODE_deconnecter/frm_genre.Designer.cs
@@ -43,6 +43,7 @@ namespace MODE_deconnecter
             this.btnsuprimer = new System.Windows.Forms.Button();
             this.btnvider = new System.Windows.Forms.Button();
             this.dgvgenre = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvgenre)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,9 +51,9 @@ namespace MODE_deconnecter
             // 
             this.lblcodedugenre.AutoSize = true;
             this.lblcodedugenre.Font = new System.Drawing.Font("Lucida Bright", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblcodedugenre.Location = new System.Drawing.Point(34, 30);
+            this.lblcodedugenre.Location = new System.Drawing.Point(38, 38);
             this.lblcodedugenre.Name = "lblcodedugenre";
-            this.lblcodedugenre.Size = new System.Drawing.Size(166, 22);
+            this.lblcodedugenre.Size = new System.Drawing.Size(189, 24);
             this.lblcodedugenre.TabIndex = 0;
             this.lblcodedugenre.Text = "CODE DU GENRE";
             // 
@@ -60,35 +61,35 @@ namespace MODE_deconnecter
             // 
             this.lblintitulegenre.AutoSize = true;
             this.lblintitulegenre.Font = new System.Drawing.Font("Lucida Bright", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblintitulegenre.Location = new System.Drawing.Point(11, 65);
+            this.lblintitulegenre.Location = new System.Drawing.Point(12, 81);
             this.lblintitulegenre.Name = "lblintitulegenre";
-            this.lblintitulegenre.Size = new System.Drawing.Size(200, 22);
+            this.lblintitulegenre.Size = new System.Drawing.Size(230, 24);
             this.lblintitulegenre.TabIndex = 2;
             this.lblintitulegenre.Text = "INTITULE DU GENRE";
             // 
             // txtcodegenre
             // 
-            this.txtcodegenre.Location = new System.Drawing.Point(288, 29);
+            this.txtcodegenre.Location = new System.Drawing.Point(324, 36);
             this.txtcodegenre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtcodegenre.Name = "txtcodegenre";
-            this.txtcodegenre.Size = new System.Drawing.Size(139, 22);
+            this.txtcodegenre.Size = new System.Drawing.Size(156, 26);
             this.txtcodegenre.TabIndex = 3;
             // 
             // txtintitulegenre
             // 
-            this.txtintitulegenre.Location = new System.Drawing.Point(288, 63);
+            this.txtintitulegenre.Location = new System.Drawing.Point(324, 79);
             this.txtintitulegenre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtintitulegenre.Name = "txtintitulegenre";
-            this.txtintitulegenre.Size = new System.Drawing.Size(139, 22);
+            this.txtintitulegenre.Size = new System.Drawing.Size(156, 26);
             this.txtintitulegenre.TabIndex = 4;
             // 
             // btnpremier
             // 
             this.btnpremier.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnpremier.Location = new System.Drawing.Point(113, 111);
+            this.btnpremier.Location = new System.Drawing.Point(127, 139);
             this.btnpremier.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnpremier.Name = "btnpremier";
-            this.btnpremier.Size = new System.Drawing.Size(51, 27);
+            this.btnpremier.Size = new System.Drawing.Size(57, 34);
             this.btnpremier.TabIndex = 5;
             this.btnpremier.Text = "<<";
             this.btnpremier.UseVisualStyleBackColor = true;
@@ -97,10 +98,10 @@ namespace MODE_deconnecter
             // btndernier
             // 
             this.btndernier.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btndernier.Location = new System.Drawing.Point(311, 111);
+            this.btndernier.Location = new System.Drawing.Point(350, 139);
             this.btndernier.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btndernier.Name = "btndernier";
-            this.btndernier.Size = new System.Drawing.Size(51, 27);
+            this.btndernier.Size = new System.Drawing.Size(57, 34);
             this.btndernier.TabIndex = 6;
             this.btndernier.Text = ">>";
             this.btndernier.UseVisualStyleBackColor = true;
@@ -109,10 +110,10 @@ namespace MODE_deconnecter
             // btnsuivant
             // 
             this.btnsuivant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsuivant.Location = new System.Drawing.Point(255, 111);
+            this.btnsuivant.Location = new System.Drawing.Point(287, 139);
             this.btnsuivant.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnsuivant.Name = "btnsuivant";
-            this.btnsuivant.Size = new System.Drawing.Size(51, 27);
+            this.btnsuivant.Size = new System.Drawing.Size(57, 34);
             this.btnsuivant.TabIndex = 7;
             this.btnsuivant.Text = ">";
             this.btnsuivant.UseVisualStyleBackColor = true;
@@ -121,10 +122,10 @@ namespace MODE_deconnecter
             // btnprecedent
             // 
             this.btnprecedent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnprecedent.Location = new System.Drawing.Point(169, 111);
+            this.btnprecedent.Location = new System.Drawing.Point(190, 139);
             this.btnprecedent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnprecedent.Name = "btnprecedent";
-            this.btnprecedent.Size = new System.Drawing.Size(51, 27);
+            this.btnprecedent.Size = new System.Drawing.Size(57, 34);
             this.btnprecedent.TabIndex = 8;
             this.btnprecedent.Text = "<";
             this.btnprecedent.UseVisualStyleBackColor = true;
@@ -133,10 +134,10 @@ namespace MODE_deconnecter
             // btnajouter
             // 
             this.btnajouter.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnajouter.Location = new System.Drawing.Point(30, 167);
+            this.btnajouter.Location = new System.Drawing.Point(34, 209);
             this.btnajouter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnajouter.Name = "btnajouter";
-            this.btnajouter.Size = new System.Drawing.Size(134, 46);
+            this.btnajouter.Size = new System.Drawing.Size(151, 58);
             this.btnajouter.TabIndex = 9;
             this.btnajouter.Text = "AJOUTER";
             this.btnajouter.UseVisualStyleBackColor = true;
@@ -145,10 +146,10 @@ namespace MODE_deconnecter
             // btnenregistrer
             // 
             this.btnenregistrer.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnenregistrer.Location = new System.Drawing.Point(98, 270);
+            this.btnenregistrer.Location = new System.Drawing.Point(110, 338);
             this.btnenregistrer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnenregistrer.Name = "btnenregistrer";
-            this.btnenregistrer.Size = new System.Drawing.Size(152, 54);
+            this.btnenregistrer.Size = new System.Drawing.Size(171, 68);
             this.btnenregistrer.TabIndex = 10;
             this.btnenregistrer.Text = "ENREGISTRER";
             this.btnenregistrer.UseVisualStyleBackColor = true;
@@ -157,10 +158,10 @@ namespace MODE_deconnecter
             // btnmodifier
             // 
             this.btnmodifier.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnmodifier.Location = new System.Drawing.Point(193, 219);
+            this.btnmodifier.Location = new System.Drawing.Point(217, 274);
             this.btnmodifier.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnmodifier.Name = "btnmodifier";
-            this.btnmodifier.Size = new System.Drawing.Size(141, 46);
+            this.btnmodifier.Size = new System.Drawing.Size(159, 58);
             this.btnmodifier.TabIndex = 11;
             this.btnmodifier.Text = "MODIFIER";
             this.btnmodifier.UseVisualStyleBackColor = true;
@@ -169,10 +170,10 @@ namespace MODE_deconnecter
             // btnsuprimer
             // 
             this.btnsuprimer.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsuprimer.Location = new System.Drawing.Point(30, 219);
+            this.btnsuprimer.Location = new System.Drawing.Point(34, 274);
             this.btnsuprimer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnsuprimer.Name = "btnsuprimer";
-            this.btnsuprimer.Size = new System.Drawing.Size(134, 46);
+            this.btnsuprimer.Size = new System.Drawing.Size(151, 58);
             this.btnsuprimer.TabIndex = 12;
             this.btnsuprimer.Text = "SUPPRIMER";
             this.btnsuprimer.UseVisualStyleBackColor = true;
@@ -181,10 +182,10 @@ namespace MODE_deconnecter
             // btnvider
             // 
             this.btnvider.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnvider.Location = new System.Drawing.Point(193, 167);
+            this.btnvider.Location = new System.Drawing.Point(217, 209);
             this.btnvider.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnvider.Name = "btnvider";
-            this.btnvider.Size = new System.Drawing.Size(141, 46);
+            this.btnvider.Size = new System.Drawing.Size(159, 58);
             this.btnvider.TabIndex = 13;
             this.btnvider.Text = "VIDER";
             this.btnvider.UseVisualStyleBackColor = true;
@@ -193,19 +194,31 @@ namespace MODE_deconnecter
             // dgvgenre
             // 
             this.dgvgenre.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvgenre.Location = new System.Drawing.Point(447, 48);
+            this.dgvgenre.Location = new System.Drawing.Point(503, 60);
             this.dgvgenre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvgenre.Name = "dgvgenre";
             this.dgvgenre.RowHeadersWidth = 62;
             this.dgvgenre.RowTemplate.Height = 28;
-            this.dgvgenre.Size = new System.Drawing.Size(401, 245);
+            this.dgvgenre.Size = new System.Drawing.Size(451, 306);
             this.dgvgenre.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(287, 336);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 68);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "ENREGISTRER";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frm_genre
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 360);
+            this.ClientSize = new System.Drawing.Size(966, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dgvgenre);
             this.Controls.Add(this.btnvider);
             this.Controls.Add(this.btnsuprimer);
@@ -246,5 +259,6 @@ namespace MODE_deconnecter
         private System.Windows.Forms.Button btnsuprimer;
         private System.Windows.Forms.Button btnvider;
         private System.Windows.Forms.DataGridView dgvgenre;
+        private System.Windows.Forms.Button button1;
     }
 }
